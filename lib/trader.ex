38e63c651925e649369c473093faae25733ad0e9
Mikael Jagekrans <mikael.jagekrans@magine.com>
2017-03-25 17:22:40 +0100

    added stochastic values

--- lib/trader.ex
+++ lib/trader.ex
@@ -1,8 +1,17 @@
 defmodule Trader do
-  @url "http://www.alphavantage.co/query?function=MACD&symbol=MSFT&interval=30min&series_type=close&apikey="
-
   def calculate do
-    formatted_macds = get_macd() |> format_response()
+    macds = MACD.get_current()
+    stochs = STOCH.get_current()
+
+    { macds, stochs } |> IO.inspect
+  end
+end
+
+defmodule MACD do
+  @url "http://www.alphavantage.co/query?function=MACD&symbol=MSFT&interval=15min&series_type=close&apikey="
+
+  def get_current do
+    get_macd() |> format_response() |> Enum.sort(& &1[:date] > &2[:date])
   end
 
   defp get_macd do
@@ -13,7 +22,6 @@ defmodule Trader do
   defp format_response(macd_source) do
     macd_source |> Map.keys
     |> Enum.map(fn date -> 
-      macd_source[date] |> IO.inspect
       %{
         "MACD" => macd,
         "MACD_Hist" => macd_hist,
@@ -24,6 +32,34 @@ defmodule Trader do
         macd: macd |> String.to_float,
         macd_hist: macd_hist |> String.to_float,
         macd_signal: macd_signal |> String.to_float
+      }}
+    end)
+  end
+end
+
+defmodule STOCH do
+  @url "http://www.alphavantage.co/query?function=STOCH&symbol=MSFT&interval=15min&slowkmatype=1&slowdmatype=1&apikey="
+
+  def get_current do
+    get_stoch() |> format_response() |> Enum.sort(& &1[:date] > &2[:date])
+  end
+
+  def get_stoch do
+    %{body: body} = HTTPoison.get!(@url <> System.get_env("AV_API_KEY"))
+    ((body |> Poison.decode!())["Technical Analysis: STOCH"])
+  end
+
+  def format_response(stoch_source) do
+    stoch_source |> Map.keys
+    |> Enum.map(fn date ->
+      %{
+        "SlowD" => slowD,
+        "SlowK" => slowK
+      } = stoch_source[date]
+
+      %{date: date, value: %{
+        slowD: slowD |> String.to_float,
+        slowK: slowK |> String.to_float
       }}
     end)
   end
