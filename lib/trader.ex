3a5b5c1aa33c24aaf2bb5b264727bb997a4c701d
Mikael Jagekrans <mikael.jagekrans@magine.com>
2017-03-25 17:40:26 +0100

    added rsi values

--- lib/trader.ex
+++ lib/trader.ex
@@ -2,8 +2,9 @@ defmodule Trader do
   def calculate do
     macds = MACD.get_current()
     stochs = STOCH.get_current()
+    rsis = RSI.get_current()
 
-    { macds, stochs } |> IO.inspect
+    { macds, stochs, rsis } |> IO.inspect
   end
 end
 
@@ -53,14 +54,35 @@ defmodule STOCH do
     stoch_source |> Map.keys
     |> Enum.map(fn date ->
       %{
-        "SlowD" => slowD,
-        "SlowK" => slowK
+        "SlowD" => slow_d,
+        "SlowK" => slow_k
       } = stoch_source[date]
 
       %{date: date, value: %{
-        slowD: slowD |> String.to_float,
-        slowK: slowK |> String.to_float
+        slow_d: slow_d |> String.to_float,
+        slow_k: slow_k |> String.to_float
       }}
+    end)
+  end
+end
+
+defmodule RSI do
+  @url "http://www.alphavantage.co/query?function=RSI&symbol=MSFT&interval=15min&time_period=10&series_type=close&apikey="
+
+  def get_current do
+    get_rsi() |> format_response() |> Enum.sort(& &1[:date] > &2[:date])
+  end
+
+  def get_rsi do
+    %{body: body} = HTTPoison.get!(@url <> System.get_env("AV_API_KEY"))
+    ((body |> Poison.decode!())["Technical Analysis: RSI"])
+  end
+
+  def format_response(rsi_source) do
+    rsi_source |> Map.keys
+    |> Enum.map(fn date ->
+      %{ "RSI" => rsi } = rsi_source[date]
+      %{date: date, value: %{ rsi: rsi |> String.to_float }}
     end)
   end
 end
